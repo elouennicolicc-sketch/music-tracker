9798332fdfaaab8929f45cef31e1920b207916bd
Elouen Nicolic <elouen.nicolicc@gmail.com>
2026-08-19 17:19:03 +0200

    fix port railway

--- app.py
+++ app.py
@@ -242,4 +242,5 @@ def logout():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port, debug=True)
